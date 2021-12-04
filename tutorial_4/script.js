88ed297da373d1f8e5edb556ecac1a01a84d6459
Misha Pinaev <92200875+classik1337@users.noreply.github.com>
2021-12-04 11:46:19 +0500

    1111

--- tutorial_4/script.js
+++ tutorial_4/script.js
@@ -1,7 +1,23 @@
-var a = 1;
-var b = '2';
-var c = a+b;
-var d = false;
-if (a === 1) {console.log('!!!');}
+var catNames = ['pushok','marsik', 'mafik', 'tusik'];
 
-else {console.log('...');}
+var arrayCat =[];
+
+for (var i = 0; i < random(1, 12); i++) {
+var cat ={
+	name: catNames[random(0, catNames.length - 1)],
+	color: 'orange',
+	len: 30
+};
+arrayCats.push(cat);
+}
+for (var i = 0;i < arrayCats.length; i++) {
+	console.log(arrayCats[i]);
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max-min + 1)) + min;
+}
+
+console.log(random(0, 5));
+
+
