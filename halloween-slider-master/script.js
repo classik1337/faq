abe28acf51a781b47fcc9b4a621563be1d9cd6fd
Misha Pinaev <92200875+classik1337@users.noreply.github.com>
2022-02-19 11:41:28 +0500

    378631

---
+++ halloween-slider-master/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    console.log('ready');
+
+    $('mark_1').click(function(){
+    $('#slide_1').show()
+    $('#slide_2').show()
+    });
+    $('mark_2').click(function(){
+        $('#slide_1').show()
+        $('#slide_2').show()
+        });
+
+});
